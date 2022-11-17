830f99b6d4ebd4e3ccadd188b0302c2111efb63c
2DC <kkts9308@gmail.com>
2022-11-17 18:09:23 +0900

    app.js 기능 개발
    html에 최종적으로 js를 적용시켜주는 app.js 기능을 개발함.

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,10 @@
-const leftBtn = document.querySelector('.btn-carousel.left')
-const rightBtn = document.querySelector('.btn-carousel.right')
-const artDisplay = document.querySelector('.cont-carousel-art')
+import fetchArt from "./fetchArt.js"
+import renderCarouselButton from "./renderCarouselButton.js"
+import clickArt from "./clickArt.js"
 
-let index = 0
 
-leftBtn.addEventListener('click', () => {
-    if (index === 0) return
-    index -= 1
-    artDisplay.style.transform = `translateX(-${190*index}px)`
-})
 
-rightBtn.addEventListener('click', () => {
-    if (index === 2) {
-        index = -1
-    }
-    index += 1
-    artDisplay.style.transform = `translateX(-${190*index}px)`})
+await fetchArt()
+renderCarouselButton()
+clickArt()
 
-/*
-디스플레이는 디스플레이대로 뿌리되
-모든 그림을 렌더할때는 별도의 class와 이벤트를 달고 나오게끔 먼저 설계할 필요성 있음.
-렌더하는 js를 어떻게 설계할 것인가??
-(요즘 머리가 잘 안돌아간다...)
-*/
